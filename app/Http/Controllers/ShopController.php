5ada6e28fd3de980349678ae03ecfddcb4141499
ms0680146 <ms0680146@gmail.com>
2019-06-14 07:40:36 +0000

    Add Category In Controller & Blade

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -4,13 +4,31 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Product;
+use App\Category;
 
 class ShopController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $products = Product::inRandomOrder()->take(12)->get();
-        return view('shop', compact('products'));
+        if ($request->category) {
+            $categoryName = $request->category;
+            $products = Product::with('categories')->wherehas('categories', function($query) use($categoryName) {
+                $query->where('slug', $categoryName);
+            })->get(); 
+        } else {
+            $categoryName = 'feature';
+            $products = Product::inRandomOrder()->take(12)->get();
+        }
+
+        $categories = Category::all();
+
+        if ($request->sort === 'low_high') {
+            $products = $products->sortBy('price');
+        } elseif ($request->sort == 'high_low') {
+            $products = $products->sortByDesc('price');
+        }
+        
+        return view('shop', compact('products', 'categories', 'categoryName'));
     }
 
     public function show(string $slug)
