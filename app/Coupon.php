dec6f94eb308b4da66a84802fbd45b478b98fd52
ms-680146 <ms0680146@gmail.com>
2019-06-17 05:08:25 +0000

    Add Coupon Model & Migration

---
+++ app/Coupon.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Coupon extends Model
+{
+    const SALE_TYPE_PRICE = 1;
+    const SALE_TYPE_DISCOUNT = 2;
+
+    public function findByCode(string $code)
+    {
+        return self::where('code', $code)->first();
+    }
+
+    public function discount($total)
+    {
+        switch($this->sale_type) {
+            case self::SALE_TYPE_PRICE:
+                $salePrice = (int) $this->sale_string;
+                break;
+            case self::SALE_TYPE_DISCOUNT:
+                $salePrice = ((int) $this->sale_string) * $total;
+                break;
+            default:
+                $salePrice = 0;    
+        }
+        return $salePrice;
+    }
+}
---
+++ database/seeds/CouponsTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Coupon;
+
+class CouponsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Coupon::create([
+            'code' => 'TEST123',
+            'sale_type' => Coupon::SALE_TYPE_PRICE,
+            'sale_string' => '100',
+        ]);
+
+        Coupon::create([
+            'code' => 'TEST12345',
+            'sale_type' => Coupon::SALE_TYPE_DISCOUNT,
+            'sale_string' => '0.5',
+        ]);
+    }
+}
