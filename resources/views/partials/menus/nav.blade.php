140f77fd1819b7fc014b2e36cb7f87d31ca3286d
ms-680146 <ms0680146@gmail.com>
2019-06-17 12:43:35 +0000

    Fix Blade Include

---
+++ resources/views/partials/menus/nav.blade.php
@@ -0,0 +1,11 @@
+<header>
+    <div class="top-nav container">
+        <div class="top-nav-left">
+            <div class="logo">電子商務</div>
+            @include('partials.menus.main')
+        </div>
+        <div>
+            @include('partials.menus.main-right')
+        </div>
+    </div> <!-- end top-nav -->
+</header>
